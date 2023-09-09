4573c7c5221744daa4d1ef1899713df4f3cc6bbe
Hosein Habibiyan <hn.habibiyan@gmail.com>
2023-09-09 11:23:29 +0330

    bugfix di inject identity db context

--- src/Modules/CleanArc.Identity/Infrastructure/Identity/Store/AppUserStore.cs
+++ src/Modules/CleanArc.Identity/Infrastructure/Identity/Store/AppUserStore.cs
@@ -1,12 +1,13 @@
-﻿using CleanArc.Identity.Domain;
+﻿using CleanArc.Identity.Data;
+using CleanArc.Identity.Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace CleanArc.Identity.Infrastructure.Identity.Store;
 
-public class AppUserStore : UserStore<User, Role, IdentityDbContext, int, UserClaim, UserRole, UserLogin, UserToken, RoleClaim>
+public class AppUserStore : UserStore<User, Role, IdentityAppDbContext, int, UserClaim, UserRole, UserLogin, UserToken, RoleClaim>
 {
-	public AppUserStore(IdentityDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
+	public AppUserStore(IdentityAppDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
 	{
 	}
 }
--- src/Modules/CleanArc.Identity/Infrastructure/Identity/Store/RoleStore.cs
+++ src/Modules/CleanArc.Identity/Infrastructure/Identity/Store/RoleStore.cs
@@ -1,12 +1,13 @@
-﻿using CleanArc.Identity.Domain;
+﻿using CleanArc.Identity.Data;
+using CleanArc.Identity.Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace CleanArc.Identity.Infrastructure.Identity.Store;
 
-public class RoleStore : RoleStore<Role, IdentityDbContext, int, UserRole, RoleClaim>
+public class RoleStore : RoleStore<Role, IdentityAppDbContext, int, UserRole, RoleClaim>
 {
-	public RoleStore(IdentityDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
+	public RoleStore(IdentityAppDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
 	{
 	}
 }
